1fda3d48a27cc3c6349fd78931731e930ee25832
JpLimags <joaopedro66.fortal@gmail.com>
2022-10-26 13:13:59 -0300

    Pequena modificação

--- Python/POO/Atvidade1.py
+++ Python/POO/Atvidade1.py
@@ -1,34 +1,27 @@
 
 class Bicicletaria:
 
-    def __init__(self, cor, modelo, ano, valor, buzinar = True, parar = True, correr = True):
+    def __init__(self, cor, modelo, ano, valor):
 
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
-        self.buzinar = buzinar
-        self.parar = parar
-        self.correr = correr
     
     def buzinar(self):
-        if self.buzinar == False:
-            print("A bicicleta não tem buzina")
-        else:
-            print("A bicicleta tem buzina")
-
+            print("bi bi...")
+    
     def parar(self):
 
-        self.parar = False
-        print("A bike tá sem freio  melhor trocar")
+    
+        print("Freiando a bicileta")
 
 
     def correr(self):
 
-        self.correr = False
-        print("Pneu furado menor !")
+        print("Zuuuuuuuum...")
 
-bike1 = Bicicletaria("Azul", "Monark", 2016,1200, False, True, True)
+bike1 = Bicicletaria("Azul", "Monark", 2016,1200)
 
 
 print(bike1.ano)
